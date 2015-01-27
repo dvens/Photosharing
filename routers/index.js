fbec17f2b4747967e7144d7362a5f8c38d44a11e
Nieuwem007 <martijnnieuwenhuizen@icloud.com>
2015-01-27 14:19:29 +0100

    vast

--- routers/index.js
+++ routers/index.js
@@ -8,10 +8,41 @@ router.get('/', function(req, res, next){
 
     connection.query('SELECT * FROM photos ORDER BY id DESC', function(err, photos){
       if(err){ return next(err); }
-      res.render('home/index', {photos: photos, req: req});
+
+       	connection.query('SELECT * FROM comments', function(err, comments){
+	      if(err){ return next(err); }
+	      	 res.render('home/index', {comments: comments ,photos: photos, req: req});
+	      });
     });
   });
 
 });
 
+router.post('/:id/comment', function(req, res) {
+	var index = req.params.id;
+	var d = new Date();
+	var yy = d.getFullYear();
+	var mm = d.getMonth();
+	var dd = d.getDay();
+	var date =  mm+'/'+dd+'/'+yy;
+
+	var comment = req.body.comment;
+	
+	req.getConnection(function(err, connection){
+      	if(err){ next(err); }
+
+		    connection.query('INSERT INTO comments (photo_id, created_at, comment) VALUES (?)', [[index, date, comment]], function(){
+		    	res.redirect('/index');
+		    });
+	});
+	
+
+});
+
 module.exports = router;
+
+
+
+ // connection.query('INSERT INTO comments (photo_id, created_at, comment) VALUES (?)', [[index, date, comment]], function(){
+	//     	res.send(index + date + comment);
+	//     });
